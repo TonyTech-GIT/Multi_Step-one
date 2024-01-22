e042ec912fa35db5af3f9e8546d151d3026b63ff
TonyTech-GIT <toomgbebu@gmail.com>
2024-01-22 12:51:44 +0100

    first commit on project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Personal from "./components/Personal"
+
+
+const App = () => {
+
+
+  return (
+    <div className="home">
+      {/* <h1>Hello World</h1> */}
+
+      <Personal />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Personal.jsx
@@ -0,0 +1,79 @@
+// import bg from '../assets/images/bg-sidebar-mobile.svg'
+
+
+const Personal = () => {
+    return (
+        <>
+            <section className='personal'>
+                <div className="personal__bg">
+                    <div className="personal__bg-nums">
+                        <span>1</span>
+                        <span>2</span>
+                        <span>3</span>
+                        <span>4</span>
+                    </div>
+                </div>
+                <div className="form-desk">
+                    <div className="desk-img">
+                        <div >
+                            <div className="num-details">
+                                <span>1</span>
+                                <div>
+                                    <p>STEP 1</p>
+                                    <h5>YOUR INFO</h5>
+                                </div>
+                            </div>
+                            <div className="num-details">
+                                <span>2</span>
+                                <div>
+                                    <p>STEP 2</p>
+                                    <h5>SELECT PLAN</h5>
+                                </div>
+                            </div>
+                            <div className="num-details">
+                                <span>3</span>
+                                <div>
+                                    <p>STEP 3</p>
+                                    <h5>ADD-ONS</h5>
+                                </div>
+                            </div>
+                            <div className="num-details">
+                                <span>4</span>
+                                <div>
+                                    <p>STEP 4</p>
+                                    <h5>SUMMARY</h5>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="personal__form">
+                        <h2>Personal info</h2>
+                        <p>Please provide your name, email, address, and phone number.</p>
+                        <label className="field">
+                            Name
+
+                        </label>
+                        <input type="text" placeholder="e.g. Stephen King" />
+                        <label className="field">
+                            Email Address
+
+                        </label>
+                        <input type="text" placeholder="e.g. stephenking@lorem.com" />
+                        <label className="field">
+                            Phone Number
+
+                        </label>
+                        <input type="text" placeholder="e.g. +1 234 567 890" />
+                    </div>
+                </div>
+
+            </section>
+
+            <button className="btn">Next Step</button>
+        </>
+
+    )
+}
+
+export default Personal
